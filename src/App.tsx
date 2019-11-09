a18bf1e8e049472de08f673ddeca49198c41fcfc
0Lucifer0 <erwan.joly@gmail.com>
2019-11-10 01:01:26 +1300

    fix and more login

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,36 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { MenuBar } from './menu-bar/menu-bar';
 import { LeftPanel } from './left-panel/left-panel';
 import { NewsPanel } from './news-panel/news-panel';
+import { AuthInformation } from './auth/auth-client';
 
-const App: React.FC = () => {
-  return (
-    <div className='App'>
-      <MenuBar />
-      <div className='container'>
-        <div className='row'>
+class App extends React.Component<{}, AuthInformation> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = { token: '', platformGameAccountId: '', user: '' };
+  }
+
+  getAuthInfo = (state: AuthInformation) => {
+    console.log(state.user);
+    this.setState(state);
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <MenuBar authCallBack={this.getAuthInfo.bind(this)} />
+        <div className='container'>
           <div className='row'>
-            <div className='col-7'> <LeftPanel /></div>
-            <div className='col-5'><NewsPanel /></div>
+            <div className='row'>
+              <div className='col-7'> <LeftPanel {...this.state} /></div>
+              <div className='col-5'><NewsPanel /></div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
 
 export default App;
--- src/rpc/client-library.tsx
+++ src/rpc/client-library.tsx
@@ -1,70 +1,22 @@
 import http from 'http';
 import { Guid } from 'guid-typescript';
+import { AuthInformation } from '../auth/auth-client';
 
 export class ClientLibrary {
-  user: string;
-  password: string;
-  language: string;
   url: string;
-  platformGameAccountId: string;
-  token: string;
+  authInfo: AuthInformation;
   port: number;
   path: string;
   code: string;
   installationId: string;
 
-  constructor(user: string, password: string, language: string, url: string, path: string, port: number) {
-    this.user = user;
-    this.password = password;
-    this.language = language;
+  constructor(url: string, path: string, port: number, authInfo: AuthInformation) {
     this.url = url;
-    this.platformGameAccountId = '';
-    this.token = '';
+    this.authInfo = authInfo;
     this.port = port;
     this.path = path;
     this.code = '';
     this.installationId = '';
-  }
-
-  getSessionToken = async () => {
-    return new Promise((resolve, reject) => {
-      process.env['_TNT_SESSION_ID'] = Guid.create().toString();
-
-      const data = JSON.stringify({
-        gfLang: this.language.substring(0, 2),
-        identity: this.user,
-        locale: this.language,
-        password: this.password,
-        platformGameId: 'dd4e22d6-00d1-44b9-8126-d8b40e0cd7c9'
-      });
-
-      const options = {
-        hostname: this.url,
-        port: this.port,
-        path: this.path + '/sessions',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': data.length
-        }
-      };
-
-      const req = http.request(options, (res) => {
-        res.on('data', (res) => {
-          const obj = JSON.parse(res);
-          this.token = obj.token;
-          this.platformGameAccountId = obj.platformGameAccountId;
-          resolve(res);
-        });
-
-        req.on('error', err => {
-          reject(err);
-        });
-      });
-
-      req.write(data);
-      req.end();
-    });
   }
 
   getAuthorizationCode = async () => {
@@ -87,7 +39,7 @@ export class ClientLibrary {
       });
 
       const data = JSON.stringify({
-        PlatformGameAccountId: this.platformGameAccountId
+        PlatformGameAccountId: this.authInfo.platformGameAccountId
       });
 
       const options = {
@@ -98,7 +50,7 @@ export class ClientLibrary {
         headers: {
           'TNT-Installation-Id': this.installationId,
           'User-Agent': 'TNTClientMS2/1.3.39',
-          'Authorization': `Bearer ${this.token}`,
+          'Authorization': `Bearer ${this.authInfo.token}`,
           'Content-Type': 'application/json',
           'Content-Length': data.length
         }
@@ -122,13 +74,10 @@ export class ClientLibrary {
   }
 
   QueryGameAccountName = (sessionId: string) => {
-    return this.user;
+    return this.authInfo.user;
   }
 
   QueryAuthorizationCode = async (sessionId: string) => {
-    if (this.token === '') {
-      await this.getSessionToken();
-    }
     await this.getAuthorizationCode();
     return this.code;
   }
